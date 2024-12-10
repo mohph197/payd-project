b5a0570f6ff0abae8659f5630e7ff4c6b7324246
Mohammed Tirichine <mohtiri718@gmail.com>
2024-12-10 12:17:22 +0100

    Small change

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,3 +1,4 @@
+import AnnonymousLayout from "@/Layouts/AnonymousLayout";
 import { PageProps } from "@/types";
 import { Head, Link } from "@inertiajs/react";
 
@@ -18,37 +19,11 @@ export default function Welcome({
     };
 
     return (
-        <>
+        <AnnonymousLayout>
             <Head title="Welcome" />
-            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-                <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                    <nav className="-mx-3 flex justify-end">
-                        {auth.user ? (
-                            <Link
-                                href={route("dashboard")}
-                                className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Your Forms
-                            </Link>
-                        ) : (
-                            <>
-                                <Link
-                                    href={route("login")}
-                                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                >
-                                    Log in
-                                </Link>
-                                <Link
-                                    href={route("register")}
-                                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                >
-                                    Register
-                                </Link>
-                            </>
-                        )}
-                    </nav>
-                </div>
-            </div>
-        </>
+            <h1 className="text-4xl font-bold text-center text-black/50 h-96 flex justify-center items-center">
+                Welcome
+            </h1>
+        </AnnonymousLayout>
     );
 }
